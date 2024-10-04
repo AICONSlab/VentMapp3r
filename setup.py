5da7da871d582945aa9f4c629144e73c732be9d7
rowanreader <16jh12@queensu.ca>
2024-10-04 13:45:06 -0400

    pinned package requirements

--- setup.py
+++ setup.py
@@ -35,9 +35,9 @@ setup(
     dependency_links=[
         'git+https://github.com/keras-team/keras-contrib.git'
     ],
+    
     install_requires=[
-        'nibabel==3.2.2', 'nipype==1.7.1', 'argparse==1.4.0', 'argcomplete==3.1.2', 'joblib==1.1.1', 'keras==2.2.4', 'nilearn==0.9.2', 'scikit-learn==0.24.2',
-        'keras-contrib==2.0.8', 'pandas==1.1.5', 'numpy==1.19.5', 'plotly==5.18.0', 'PyQt5==5.15.6', 'termcolor==1.1.0'
+        'PyQt5==5.15.6', 'PyQt5-Qt5==5.15.15', 'PyQt5-sip==12.9.1', 'argcomplete==3.1.2', 'argparse=1.4.0', 'bleach==1.5.0', 'cached-property==1.5.2', 'certifi==2024.8.30', 'charset-normalizer==2.0.12', 'ci-info==0.2.0', 'click==8.0.4', 'dataclasses==0.8', 'decorator==4.4.2', 'enum34==1.1.10', 'etelemetry==0.2.2', 'filelock==3.4.1', 'h5py==3.1.0', 'html5lib==0.9999999', 'idna==3.10', 'importlib-metadata==4.8.3', 'isodate==0.6.1', 'joblib==1.1.1', 'keras==2.2.4', 'keras-applications==1.0.8', 'keras-contrib==2.0.8', 'keras-preprocessing==1.1.2', 'lxml==5.3.0', 'markdown==3.3.7', 'networkx==2.5.1', 'nibabel==3.2.2', 'nilearn==0.9.2', 'nipype==1.7.1', 'numpy==1.19.5', 'packaging==21.3', 'pandas==1.1.5', 'plotly==5.18.0', 'protobuf==3.19.6', 'prov==2.0.1', 'pydot==1.4.2', 'pyparsing==3.1.4', 'python-dateutil==2.9.0.post0', 'pytz==2024.2', 'pyyaml==6.0.1', 'rdflib==5.0.0', 'requests==2.27.1', 'scikit-learn==0.24.2', 'scipy==1.5.4', 'simplejson==3.19.3', 'six==1.16.0', 'tenacity==8.2.2', 'tensorflow==1.4.0', 'tensorflow-tensorboard==0.4.0', 'termcolor==1.1.0', 'threadpoolctl==3.1.0', 'traits==6.4.1', 'typing-extensions==4.1.1', 'urllib3==1.26.20', 'werkzeug==2.0.3', 'zipp==3.6.0'
     ],
     extras_require={
         "ventmapper": ["tensorflow==1.4.0"],
